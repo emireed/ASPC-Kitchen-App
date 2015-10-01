b6314877c78568e36cf27f0b2321d098821672a8
KOverbay <koverbay@hmc.edu>
2015-10-01 13:25:36 -0700

    Parse Connected
    Set up basic Parse connectivity. Created first ParseObject.

---
+++ app/src/main/java/com/example/emi/aspckitchen/ASPCKitchenApplication.java
@@ -0,0 +1,19 @@
+package com.example.emi.aspckitchen;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Keighley on 9/30/2015.
+ */
+public class ASPCKitchenApplication extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this, "z4iOYPd7anE4HWUYUnICtEcsr2sAcZoOGu7xuqby", "fbgMzfTb3TWb5T8v7Juf1cKtBdpP2zgl8rxbz8zG");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
